d49232a9601f36f29598f229fb5f29b13a8802eb
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-11-07 14:44:55 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,11 +7,6 @@ terraform {
   }
 }
 
-provider "azurerm"{
-#client_id       = var.client_id
-#client_secret   = var.client_secret
-#tenant_id       = var.tenant_id
-#subscription_id = var.subscription_id
-features{
-}
+provider "azurerm" {
+  features {}
 }
